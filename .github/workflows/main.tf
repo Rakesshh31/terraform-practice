26fa335759497da2124efc093cd6fd1fa65650ef
Rakesshh31 <rakesh.dyadav18@gmail.com>
2025-07-28 17:14:57 +0530

    Update main.tf

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,7 +1,14 @@
+provider "aws" {
+region = "us-west-1"
+}
+
+
+
 resource "aws_instance" "name" {
     ami ="ami-032db79bb5052ca0f"
     instance_type = "t2.micro"
-    
+    tags = {
+Name = "prod"
 
   
 }
